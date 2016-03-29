7c126f7f35ad4acee535c75f1d90211264a1a66a
maciek <maciej.kaminski@pwr.edu.pl>
2016-03-29 16:34:59 +0200

    Much more info about errors. Many new messages. Code cleanup.

--- qgsmongolayer.py
+++ qgsmongolayer.py
@@ -22,6 +22,8 @@ __author__ = 'Maciej Kamiński Politechnika Wrocławska'
 
 from qgis.core import QgsVectorLayer,QgsField,QgsFeature,QgsGeometry,QgsPoint,QgsMapLayerRegistry
 from PyQt4.QtCore import  QVariant
+import uuid
+
 
 class QgsMongoLayer(QgsVectorLayer):
 
@@ -29,125 +31,168 @@ class QgsMongoLayer(QgsVectorLayer):
         self.mongoConnector=mongoConnector
         self.database=database
         self.collection = collection
-        self.geometryField=geometryField
+        self.geometryField=str(geometryField)
         # I hope it fits into memory
         self.data=list(mongoConnector[database][collection].find())
+        self.idFieldCheck(self.data[0])
+        self.reference_item_id=self.data[0]["_id"]
 
-        # get all keys as columns in layer data
-        self.keysOrder=self.data[0].keys()
-        self.keysOrder.remove("_id")
-        self.keysOrder.remove(geometryField)
+        # get all keys as columns in layer data from first item
+        self.featuresKeys=self.data[0].keys()
+        if not all([type(key)==str for key in self.featuresKeys]):
+            TypeError("All attribute names in feature must be string."+
+                      "Check object "+str(self.reference_item_id))
 
-        if not self.checkCollection():
-            raise TypeError("Improper collection")
+        self.featuresKeys.remove("_id")
+        self.featuresKeys.remove(geometryField)
 
-        self.recognizedGeometry=self.getGeometryType(self.data[0][self.geometryField])
-        if self.recognizedGeometry=="point":
-            gtype="Point"
-        elif self.recognizedGeometry== "line":
-            gtype="LineString"
-        else:
-            pass
+        #lets keep types and QVariants in one place
+        self.featuresKeyType=dict([(key,self.getKeyType(key)[0])
+                                   for key in self.featuresKeys])
+        self.featuresKeyQVariant=dict([(key,self.getKeyType(key)[1])
+                                   for key in self.featuresKeys])
 
-        super(QgsMongoLayer,self). \
-            __init__(gtype,'mongo-'+self.collection,"memory")
+        # We don't know geometry Yet
+        self.geometryType=None
+
+        # We take first item geometry as layer geometry (as reference)
+        self.geometryType=self.getGeometryType(self.data[0],self.geometryField)
+
+        self.addLayer()
 
         self.startEditing()
 
-        for key in self.keysOrder:
-            self.addAttribute(QgsField(str(key),self.getKeyQVariant(key)))
+        self.setLayerAttributes()
 
+        # add layer features
         for feature in self.data:
-
             qfeature=QgsFeature()
 
-            if self.recognizedGeometry=="point":
+            self.idFieldCheck(feature)
+
+            if not self.geometryType == self.getGeometryType(feature,geometryField):
+                raise ValueError("Check geometry in object "+
+                                 str(feature["_id"])+
+                                 " or "+str(self.reference_item_id))
+
+            if self.geometryType=="point":
                 qfeature.setGeometry(QgsGeometry.fromPoint(
                     QgsPoint(*feature[geometryField])
                 ))
-            elif self.recognizedGeometry== "line":
+            elif self.geometryType== "line":
                 qfeature.setGeometry(QgsGeometry.fromPolyline([
                     QgsPoint(*pt) for pt in feature[geometryField]
                 ]))
-            else:
-                pass
-            attributes=[]
 
-            for key in self.keysOrder:
-
-                if self.getKeyQVariant(key) == QVariant.Int:
-                    attrib = int(feature[key])
-                if self.getKeyQVariant(key) == QVariant.Double:
-                    attrib = float(feature[key])
-                if self.getKeyQVariant(key) == QVariant.String:
-                    try:
-                        # we try to put lists and other strange objects to str
-                        attrib = str(feature[key])
-                    except UnicodeEncodeError as e:
-                        # if unicode we simply pass it
-                        attrib = feature[key]
-                    except:
-                        raise NotImplemented()
-
-                attributes.append(attrib)
-
-            qfeature.setAttributes(attributes)
+            qfeature.setAttributes(self.createAttributes(feature))
 
             self.addFeature(qfeature)
-        # we tnt need all those data
+
+        # we don't need all those data
         del(self.data)
+
         self.commitChanges()
 
 
-    def getKeyQVariant(self,key):
+    def idFieldCheck(self,feature):
+        """
+        Checks if feature has _id field witch is mandatory. Throws Error.
+        :param feature: feature to chceck
+        :return: None
+        """
+        if "_id" not in feature:
+            raise ValueError("'_id' field must be an objects identifier."+
+                             "Not found in "+str(self.data[0]))
+
+    def addLayer(self):
+        """
+        Creates described layer point/line
+        :return: None
+        """
+        gtype=None
+        if self.geometryType=="point":
+            gtype="Point"
+        elif self.geometryType== "line":
+            gtype="LineString"
+
+        super(QgsMongoLayer,self).\
+            __init__(gtype,
+                     self.collection+'-'+str(uuid.uuid4())[0:4],
+                     "memory")
+
+    def setLayerAttributes(self):
+        """
+        Writes attributes to layer. Writes required fields
+        :return:None
+        """
+        for key in self.featuresKeys:
+            self.addAttribute(QgsField(key,self.featuresKeyQVariant[key]))
+
+    def getKeyType(self,key):
+        """
+        checks type of element get by key
+        :param key: key
+        :return: tuple (base type , QVariant type)
+        """
 
         if type(self.data[0][key]) is int:
-            return QVariant.Int
+            return (int,QVariant.Int)
 
         if type(self.data[0][key]) is float:
-            return QVariant.Double
+            return (float,QVariant.Double)
 
-        return QVariant.String
+        # we try to put lists and other strange objects to str
+        return (str,QVariant.String)
 
-    def checkCollection(self):
+    def createAttributes(self,feature):
         """
-        Checks if keys in each mongo document are same
-        Checks if geometry in each document is same
-        :return: Thru if ok False otherwise
+        Manage attribute of features
+        :param feature: feature for witch we obtain geometry
+        :return: prepared list of attributes
         """
-        if len(self.data)>0 and \
-            all([set(self.data[0].keys())==set(element.keys())
-                for element in self.data]) and \
-            all([self.getGeometryType(self.data[0][self.geometryField])==
-                self.getGeometryType(element[self.geometryField])
-                for element in self.data]):
-            return True
-        return False
+        for field_key in self.featuresKeys:
+            if field_key not in feature.keys():
+                raise ValueError("Field "+field_key+
+                                 " missing in object "+str(feature["_id"]))
 
-    def analyzeFieldType(self,field):
-        pass
+        attributes=[]
+        for key in self.featuresKeys:
+            try:
+                attrib=self.featuresKeyType[key][0](feature[key])
+            except UnicodeEncodeError as e:
+                # if unicode we simply pass it (it's a string)
+                attrib = feature[key]
+            except:
+                raise TypeError("Can't cast attribute "+key+
+                                " in object "+str(feature['_id'])+
+                                " to type "+str(self.featuresKeyType[key]))
 
-    def getGeometryType(self,featureGeometry):
+            attributes.append(attrib)
+        return attributes
+
+    def getGeometryType(self,feature):
         """
         Try to recognize geometry
-        :param featureGeometry: one feature for witch we try to recognize geometry
+        :param feature: one feature for witch we try to recognize geometry
         :return: 'point' for point geometry 'line' for line geometry
         """
-        if len(featureGeometry)==2 and (
-                        isinstance(featureGeometry[0],int) or
-                        isinstance(featureGeometry[0],float) or
-                        isinstance(featureGeometry[1],int) or
-                        isinstance(featureGeometry[1],float)):
+        if self.geometryField not in feature:
+            raise IndexError("No geometry in object"+str(feature["_id"]))
+        if len(feature[self.geometryField])==2 and (
+                        isinstance(feature[self.geometryField][0],int) or
+                        isinstance(feature[self.geometryField][0],float) or
+                        isinstance(feature[self.geometryField][1],int) or
+                        isinstance(feature[self.geometryField][1],float)):
             return "point"
 
-        if isinstance(featureGeometry,list) and \
+        if isinstance(feature[self.geometryField],list) and \
             all([(isinstance(element,list) or isinstance(element,tuple)) and
                     len(element)==2 and (
                     isinstance(element[0],int) or
                     isinstance(element[0],float) or
                     isinstance(element[1],int) or
                     isinstance(element[1],float))
-                 for element in featureGeometry]):
+                 for element in feature[self.geometryField]]):
             return "line"
 
-        raise TypeError("Improper Geometry")
+        raise ValueError("Unknown geometry in object "+str(feature["_id"]))
